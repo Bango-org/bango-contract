417aebbef3f79707837e62c3d93eddd1d24b2c93
Swapnil Shinde <swapnilshinde9382@gmail.com>
2024-12-08 18:35:29 +0530

    init conditional tokens

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+// import {Counter} from "../src/Counter.sol";
 
 contract CounterScript is Script {
     Counter public counter;
@@ -12,8 +12,8 @@ contract CounterScript is Script {
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        // counter = new Counter();
 
-        vm.stopBroadcast();
+        // vm.stopBroadcast();
     }
 }
